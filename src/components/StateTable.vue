53eb973e76baec159349d293a50ddce54234213f
AbidemiT <abidemis01@gmail.com>
2020-05-19 05:51:11 +0100

    Added Search functionality in StateTable.vue

--- src/components/StateTable.vue
+++ src/components/StateTable.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container">
+    <div class="row mt-3 mb-3 middle">
+      <div class="col-sm-12 input-container">
+        <input type="search" v-model="searchState" placeholder="Search by State Name" />
+      </div>
+    </div>
     <div class="row">
       <div class="col-12 table-responsive">
         <table class="table table-sm table-striped">
@@ -7,14 +12,14 @@
             <tr class="table-success">
               <th scope="col">s/n</th>
               <th scope="col">State</th>
-              <th scope="col">confirmedCases</th>
-              <th scope="col">casesOnAdmission</th>
-              <th scope="col">discharged</th>
-              <th scope="col">death</th>
+              <th scope="col">Confirmed Cases</th>
+              <th scope="col">Cases On Admission</th>
+              <th scope="col">Discharged</th>
+              <th scope="col">Death</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(state, i) in states" :key="i">
+            <tr v-for="(state, i) in stateResult" :key="i">
               <th scope="row">{{i + 1}}</th>
               <td>{{state.state}}</td>
               <td>{{state.confirmedCases}}</td>
@@ -33,10 +38,47 @@
 import { mapGetters } from "vuex";
 export default {
   name: "StateTable",
+  data() {
+    return {
+      searchState: ""
+    };
+  },
   computed: {
     ...mapGetters({
       states: "totalState"
-    })
+    }),
+    stateResult() {
+      if (this.searchState) {
+        return this.states.filter(state => {
+          return this.searchState
+            .toLowerCase()
+            .split(" ")
+            .every(v => state.state.toLowerCase().includes(v));
+        });
+      } else {
+        return this.states;
+      }
+    }
   }
 };
 </script>
+
+
+<style scoped>
+.middle {
+  width: 70%;
+  margin: 0 auto;
+}
+.input-container input {
+  height: 50px;
+  border: 1px solid #ddd;
+  outline: none;
+  border-radius: 5px;
+  width: 100%;
+  padding-left: 10px;
+}
+.input-container input:focus,
+.input-container input:hover {
+  border: 1px solid #66bb6a;
+}
+</style>
